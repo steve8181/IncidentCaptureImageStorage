a14be1d79388f5cc9a1ce37eec15f078aba2a6c8
Steve8181 <Stephen.A.Smith@students.ittralee.ie>
2021-04-11 11:48:49 +0100

    Add project files.

---
+++ IncidentCaptureImageStorage/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace IncidentCaptureImageStorage
+{
+    partial class IncidentCaptureStorage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnFileSelection = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbRunningTime = new System.Windows.Forms.Label();
+            this.lbFrameNumber = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.ofdVideoFile = new System.Windows.Forms.OpenFileDialog();
+            this.IncidentImgBox = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.IncidentImgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFileSelection
+            // 
+            this.btnFileSelection.Location = new System.Drawing.Point(32, 31);
+            this.btnFileSelection.Name = "btnFileSelection";
+            this.btnFileSelection.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSelection.TabIndex = 0;
+            this.btnFileSelection.Text = "Open Video";
+            this.btnFileSelection.UseVisualStyleBackColor = true;
+            this.btnFileSelection.Click += new System.EventHandler(this.btnFileSelection_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(542, 30);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(32, 460);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // lbRunningTime
+            // 
+            this.lbRunningTime.AutoSize = true;
+            this.lbRunningTime.Location = new System.Drawing.Point(258, 465);
+            this.lbRunningTime.Name = "lbRunningTime";
+            this.lbRunningTime.Size = new System.Drawing.Size(79, 13);
+            this.lbRunningTime.TabIndex = 3;
+            this.lbRunningTime.Text = "Running Time: ";
+            // 
+            // lbFrameNumber
+            // 
+            this.lbFrameNumber.AutoSize = true;
+            this.lbFrameNumber.Location = new System.Drawing.Point(484, 465);
+            this.lbFrameNumber.Name = "lbFrameNumber";
+            this.lbFrameNumber.Size = new System.Drawing.Size(101, 13);
+            this.lbFrameNumber.TabIndex = 4;
+            this.lbFrameNumber.Text = "Number Of Frames: ";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(132, 33);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(158, 20);
+            this.txtFilePath.TabIndex = 5;
+            // 
+            // ofdVideoFile
+            // 
+            this.ofdVideoFile.FileName = "openFileDialog1";
+            // 
+            // IncidentImgBox
+            // 
+            this.IncidentImgBox.Location = new System.Drawing.Point(109, 83);
+            this.IncidentImgBox.Name = "IncidentImgBox";
+            this.IncidentImgBox.Size = new System.Drawing.Size(433, 349);
+            this.IncidentImgBox.TabIndex = 2;
+            this.IncidentImgBox.TabStop = false;
+            // 
+            // IncidentCaptureStorage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 499);
+            this.Controls.Add(this.IncidentImgBox);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lbFrameNumber);
+            this.Controls.Add(this.lbRunningTime);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFileSelection);
+            this.Name = "IncidentCaptureStorage";
+            this.Text = "Captured Incident";
+            ((System.ComponentModel.ISupportInitialize)(this.IncidentImgBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileSelection;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lbRunningTime;
+        private System.Windows.Forms.Label lbFrameNumber;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.OpenFileDialog ofdVideoFile;
+        private Emgu.CV.UI.ImageBox IncidentImgBox;
+    }
+}
+
---
+++ IncidentCaptureImageStorage/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+//emgucCV references
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace IncidentCaptureImageStorage
+{
+    public partial class IncidentCaptureStorage : Form
+    {
+
+        private Capture recording;
+        Image<Bgr, Byte> recordingFrame, recordingFrameResized;
+
+        double recordingFrameNumber = 0, recordingNoOfFrames = 0, recordingFrameRate = 0;
+
+        public IncidentCaptureStorage()
+        {
+            InitializeComponent();
+        }
+
+        private void EndProcess()
+        {
+            if (recording == null)
+                recording.Dispose();
+        }
+
+        private void VideoProcessing(object sender, EventArgs e)
+        {
+            try
+            {
+                recordingNoOfFrames = recording.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_POS_FRAMES);
+                recordingFrame = recording.QueryFrame();
+
+                if (recording != null)
+                {
+                    recordingFrameResized = recordingFrame.Resize(433, 349, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+                    IncidentImgBox.Image = recordingFrameResized;
+
+                    double timeStamp = recording.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_POS_MSEC);
+                    lbRunningTime.Text = "Time: " + TimeSpan.FromMilliseconds(timeStamp).ToString().Substring(0, 8);
+
+                    double currentFrameNumber = recording.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_POS_FRAMES);
+                    lbFrameNumber.Text = "Current Frame Number: " + recordingFrameNumber.ToString();
+
+                    Thread.Sleep((int)(1000f / recordingFrameRate));
+
+                    recordingFrame.Save(@"C:\DevOps_Project\Incident_Capture\Incident_Number_" + recordingFrameNumber + " Timestamp_" + timeStamp + ".jpg");
+
+                    recordingFrameNumber++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnFileSelection_Click(object sender, EventArgs e)
+        {
+            ofdVideoFile.FileName = "";
+
+            if (ofdVideoFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    recording = null;
+                    recording = new Capture(ofdVideoFile.FileName);
+                    recording.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, 433);
+                    recording.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, 349);
+                    recordingFrameRate = recording.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS);
+                    recordingFrameNumber = recording.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_COUNT);
+                    Application.Idle += VideoProcessing;
+                    btnStart.Text = "Stop Extraction";
+                    txtFilePath.Text = ofdVideoFile.FileName;
+                }
+                catch (NullReferenceException except)
+                {
+                    MessageBox.Show(except.Message);
+                }
+            }
+            if (btnFileSelection.Text == "Stop Extraction")
+            {
+                recording.Stop();
+
+                Application.Idle -= VideoProcessing;
+                EndProcess();
+                btnStart.Text = "Restart Extraction";
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
