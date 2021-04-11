0817950650fcea9e567bf6863ff8bb3c56af8231
Steve8181 <Stephen.A.Smith@students.ittralee.ie>
2021-04-11 14:25:25 +0100

    Added unit test

---
+++ IncidentCaptureImageStorageTests/IncidentCaptureStorageTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IncidentCaptureImageStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentCaptureImageStorage.Tests
+{
+    [TestClass()]
+    public class IncidentCaptureStorageTests
+    {
+        [TestMethod()]
+        public void IncidentCaptureStorageTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IncidentCaptureStorageTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
